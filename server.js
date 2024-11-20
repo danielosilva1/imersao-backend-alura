3f526c0da5dafd8bdc08881e04b39979db0b3fab
dosilva <danielosilva@alu.ufc.br>
2024-11-20 11:24:34 -0300

    altera rota /posts para obter todos os posts disponíveis no mongodb hospedado na nuvem

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import conectarAoBanco from "./src/config/dbConfig.js";
-
-await conectarAoBanco(process.env.STRING_CONEXAO);
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 const app = express();
 app.use(express.json()); // Possibilita que arquivos em formato JSON sejam tratados como JSON pelo servidor
@@ -10,19 +9,27 @@ app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/posts", (req, res) => {
+async function getTodosPosts() {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+}
+
+app.get("/posts", async (req, res) => {
+    const posts = await getTodosPosts();
+
     res.status(200).json(posts);
 });
 
-function buscarPostPorID(id) {
-    // Retorna o índice do post cujo id é o buscado
-    // Documentação: função findIndex retorna -1 se nenhum elemento do array satisfaz a condição
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    });
-}
+// function buscarPostPorID(id) {
+//     // Retorna o índice do post cujo id é o buscado
+//     // Documentação: função findIndex retorna -1 se nenhum elemento do array satisfaz a condição
+//     return posts.findIndex((post) => {
+//         return post.id === Number(id);
+//     });
+// }
 
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorID(req.params.id);
-    res.status(200).json(posts[index]);
-});
+// app.get("/posts/:id", (req, res) => {
+//     const index = buscarPostPorID(req.params.id);
+//     res.status(200).json(posts[index]);
+// });
